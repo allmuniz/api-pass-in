ac2d5ca00aa003c2b3bbe83421e1b1224d3078f9
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-04-03 19:27:20 -0300

    Update Swagger

--- src/main/java/com/project/passin/PassInApplication.java
+++ src/main/java/com/project/passin/PassInApplication.java
@@ -1,9 +1,18 @@
 package com.project.passin;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Pass-in",
+				description = "API responsavel pela gestão de eventos",
+				version = "1"
+		)
+)
 public class PassInApplication {
 
 	public static void main(String[] args) {
